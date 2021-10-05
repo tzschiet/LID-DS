b2f5425c7a204251d0e252e9d44613e7d61e83f0
Mlyfall <ephcs@web.de>
2021-10-05 17:41:19 +0200

    Erste Arbeitsschritte für #170

--- Tools/BaselineSTIDE.py
+++ Tools/BaselineSTIDE.py
@@ -1,9 +1,27 @@
 from Tools.data_loader import DataLoader, RecordingType
+from collections import deque
 
-
+q = deque(maxlen=3)
+sc_dict_int = {}
+sc_dict_count = {}
 
 dataloader = DataLoader("/home/eschulze/LID-DS-2021 Datensatz/CVE-2012-2122")
 for recording in dataloader.test_data():
     print(recording.name)
     for systemcall in recording.syscalls():
-        print(systemcall.name())
+
+        if not str(systemcall.name()) in sc_dict_int:
+            sc_dict_int[str(systemcall.name())] = len(sc_dict_int) + 1
+
+        q.append(sc_dict_int[str(systemcall.name())])
+
+        qtuple = tuple(q)
+
+        if len(qtuple) == 3:
+            if not qtuple in sc_dict_count:
+                sc_dict_count[qtuple] = 1
+
+            else:
+                sc_dict_count[qtuple] += 1
+
+print(sc_dict_count)
